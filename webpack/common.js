a3a7c228dd8dfb11afe33de2daa51ebf184319a1
Bryce Rutherford <bryce.rutherford@varsitytutors.com>
2020-10-25 16:59:57 -0400

    optimize webpack config

--- webpack/common.js
+++ webpack/common.js
@@ -1,37 +1,18 @@
-const paths = require('./paths');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const paths = require('./paths')
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   // Where webpack looks to start building the bundle
-  entry: {
-    main: paths.src + '/index.js'
-  },
+  entry: [paths.src + '/index.js'],
 
-  module: {
-    rules: [
-      // CSS, PostCSS, and Sass
-      {
-        test: /\.(sass|scss|css)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      // JavaScript
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      // Images
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-      },
-      // Audio
-      {
-        test: /\.(ogg|mp3|wav|mpe?g)$/i,
-        use: 'file-loader'
-      }
-    ]
+  // Where webpack outputs the assets and bundles
+  output: {
+    path: paths.build,
+    filename: '[name].bundle.js',
+    publicPath: '/',
   },
 
   // Customize the webpack build process
@@ -39,13 +20,76 @@ module.exports = {
     // Removes/cleans build folders and unused assets when rebuilding
     new CleanWebpackPlugin(),
 
+    // Copies files from target to destination folder
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: paths.public,
+          to: 'assets',
+          globOptions: {
+            ignore: ['*.DS_Store'],
+          },
+        },
+      ],
+    }),
+
     // Generates an HTML file from a template
     // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
-      title: 'platformer',
-      // favicon: paths.src + '/images/favicon.png',
+      title: 'Platformer',
       template: paths.src + '/template.html', // template file
       filename: 'index.html', // output file
-    }),
-  ]
+    })
+  ],
+
+  // Determine how modules within the project are treated
+  module: {
+    rules: [
+      // JavaScript: Use Babel to transpile JavaScript files
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+
+      // Styles: Inject CSS into the head with source maps
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+        ],
+      },
+
+      // Images: Copy image files to build folder
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource'
+      },
+
+      // Fonts and SVGs: Inline files
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline'
+      },
+
+      // Sounds
+      {
+        test: /\.(ogg|mp3|wav|mpe?g)$/i,
+        use: 'file-loader'
+      }
+    ],
+  },
 }
