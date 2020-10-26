ab840c7eb94aec838909fc6e72115b2690c0381e
Bryce Rutherford <bryce.rutherford@varsitytutors.com>
2020-10-25 20:20:31 -0400

    more concise file loading

--- src/scripts/scenes/load.js
+++ src/scripts/scenes/load.js
@@ -1,35 +1,5 @@
 // dependencies
 import Phaser from 'phaser';
-// data
-import level00 from '../../data/level00.json';
-import level01 from '../../data/level01.json';
-// images
-import backgroundImage from '../../images/background.png';
-import coinImage from '../../images/coin_icon.png';
-import grass1Image from '../../images/grass_1x1.png';
-import grass2Image from '../../images/grass_2x1.png';
-import grass4Image from '../../images/grass_4x1.png';
-import grass6Image from '../../images/grass_6x1.png';
-import grass8Image from '../../images/grass_8x1.png';
-import groundImage from '../../images/ground.png';
-import invisibleWallImage from '../../images/invisible_wall.png';
-import keyImage from '../../images/key.png';
-import numbersImage from '../../images/numbers.png';
-// audio
-import coinAudio from '../../audio/coin.wav';
-import doorAudio from '../../audio/door.wav';
-import keyAudio from '../../audio/key.wav';
-import jumpAudio from '../../audio/jump.wav';
-import stompAudio from '../../audio/stomp.wav';
-import songAudioPrimary from '../../audio/bgm.mp3';
-import songAudioSecondary from '../../audio/bgm.ogg';
-// spritesheets
-import coinSpritesheet from '../../images/coin_animated.png';
-import decorSpritesheet from '../../images/decor.png';
-import doorSpritesheet from '../../images/door.png';
-import heroSpritesheet from '../../images/hero.png';
-import keySpritesheet from '../../images/key_icon.png';
-import spiderSpritesheet from '../../images/spider.png';
 
 class Load extends Phaser.Scene {
   constructor() {
@@ -95,11 +65,13 @@ class Load extends Phaser.Scene {
     }));
 
     // sounds
-    this.sound.add('sfx:coin');
-    this.sound.add('sfx:door');
-    this.sound.add('sfx:key');
-    this.sound.add('sfx:jump');
-    this.sound.add('sfx:stomp');
+    const sfxConfig = { volume: .5 };
+
+    this.sound.add('sfx:coin', sfxConfig);
+    this.sound.add('sfx:door', sfxConfig);
+    this.sound.add('sfx:key', sfxConfig);
+    this.sound.add('sfx:jump', sfxConfig);
+    this.sound.add('sfx:stomp', sfxConfig);
     this.sound.add('song:bgm');
 
     // play music
@@ -113,52 +85,55 @@ class Load extends Phaser.Scene {
     /* load shared assets */
 
     // data
-    this.load.json('level:0', level00);
-    this.load.json('level:1', level01);
+    this.load.json('level:0', require('../../data/level00.json'));
+    this.load.json('level:1', require('../../data/level01.json'));
 
     // images
-    this.load.image('background', backgroundImage);
-    this.load.image('font:numbers', numbersImage);
-    this.load.image('grass:8x1', grass8Image);
-    this.load.image('grass:6x1', grass6Image);
-    this.load.image('grass:4x1', grass4Image);
-    this.load.image('grass:2x1', grass2Image);
-    this.load.image('grass:1x1', grass1Image);
-    this.load.image('ground', groundImage);
-    this.load.image('icon:coin', coinImage);
-    this.load.image('invisible-wall', invisibleWallImage);
-    this.load.image('key', keyImage);
+    this.load.image('background', require('../../images/background.png'));
+    this.load.image('font:numbers', require('../../images/numbers.png'));
+    this.load.image('grass:1x1', require('../../images/grass_1x1.png'));
+    this.load.image('grass:2x1', require('../../images/grass_2x1.png'));
+    this.load.image('grass:4x1', require('../../images/grass_4x1.png'));
+    this.load.image('grass:6x1', require('../../images/grass_6x1.png'));
+    this.load.image('grass:8x1', require('../../images/grass_8x1.png'));
+    this.load.image('ground', require('../../images/ground.png'));
+    this.load.image('icon:coin', require('../../images/coin_icon.png'));
+    this.load.image('invisible-wall', require('../../images/invisible_wall.png'));
+    this.load.image('key', require('../../images/key.png'));
 
     // audio
-    this.load.audio('sfx:coin', coinAudio);
-    this.load.audio('sfx:door', doorAudio);
-    this.load.audio('sfx:key', keyAudio);
-    this.load.audio('sfx:jump', jumpAudio);
-    this.load.audio('sfx:stomp', stompAudio);
-    this.load.audio('song:bgm', [songAudioPrimary, songAudioSecondary]);
+    this.load.audio('sfx:coin', require('../../audio/coin.wav').default);
+    this.load.audio('sfx:door', require('../../audio/door.wav').default);
+    this.load.audio('sfx:key', require('../../audio/key.wav').default);
+    this.load.audio('sfx:jump', require('../../audio/jump.wav').default);
+    this.load.audio('sfx:stomp', require('../../audio/stomp.wav').default);
+    this.load.audio('song:bgm', [
+      require('../../audio/bgm.mp3').default,
+      require('../../audio/bgm.ogg').default
+    ]);
 
     // spritesheets
-    this.load.spritesheet('coin', coinSpritesheet, {
+    this.load.spritesheet('coin', require('../../images/coin_animated.png'), {
       frameHeight: 22,
       frameWidth: 22
     });
-    this.load.spritesheet('decoration', decorSpritesheet, {
+    this.load.spritesheet('decoration', require('../../images/decor.png'), {
       frameHeight: 42,
       frameWidth: 42
     });
-    this.load.spritesheet('door', doorSpritesheet, {
+    this.load.spritesheet('door', require('../../images/door.png'), {
       frameHeight: 66,
       frameWidth: 42
     });
-    this.load.spritesheet('hero', heroSpritesheet, {
+    this.load.spritesheet('hero', require('../../images/hero.png'), {
       frameHeight: 42,
       frameWidth: 36
     });
-    this.load.spritesheet('icon:key', keySpritesheet, {
+    this.load.spritesheet('icon:key', require('../../images/key_icon.png'), {
       frameHeight: 30,
       frameWidth: 34
     });
-    this.load.spritesheet('spider', spiderSpritesheet, {
+    this.load.spritesheet('spider', require('../../images/spider.png'), {
       frameHeight: 32,
       frameWidth: 42
     });
